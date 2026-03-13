dbd477737a17cda05fff9d61247e91760ec425b6
jeffnts <jeffersonsouza.hubxp@parceiro.afya.com.br>
2026-03-13 10:23:40 -0300

    feat: initial migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260313_120828_baseline from './20260313_120828_baseline';
+import * as migration_20260313_132325 from './20260313_132325';
 
 export const migrations = [
   {
-    up: migration_20260313_120828_baseline.up,
-    down: migration_20260313_120828_baseline.down,
-    name: '20260313_120828_baseline'
+    up: migration_20260313_132325.up,
+    down: migration_20260313_132325.down,
+    name: '20260313_132325'
   },
 ];
