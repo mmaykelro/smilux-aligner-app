7d60b292333182fe05435c01973ffaf381c99fca
jeffnts <jeffersonsouza.hubxp@parceiro.afya.com.br>
2026-03-13 14:53:17 -0300

    fix: migrations

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260313_132325 from './20260313_132325';
+import * as migration_20260313_170237 from './20260313_170237';
 
 export const migrations = [
   {
-    up: migration_20260313_132325.up,
-    down: migration_20260313_132325.down,
-    name: '20260313_132325'
+    up: migration_20260313_170237.up,
+    down: migration_20260313_170237.down,
+    name: '20260313_170237'
   },
 ];
